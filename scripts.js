cafcd51f9330ae691401a665fcf613e78d2b8948
CarlosMartins <carlosmartins00098>
2023-09-11 20:00:24 -0300

    criando o arquivo scripts.js

---
+++ scripts.js
@@ -0,0 +1,26 @@
+//variáveis var, let e const 
+let idade = 20;
+let nome = "Carlos"
+const gravidade = 9.8;
+
+//gravidade = 10; a constante não varia
+
+//console.log(nome);
+//console.log(gravidade);
+
+
+function soma(numero1, numero2) {
+  return numero2 + numero1
+}
+
+//console.log(soma(4, 9));
+
+function parabens() {
+  console.log("Parabéns")
+}
+
+//parabens();
+
+const frutas = ["manga", "maça", "uva"];
+
+console.log();
